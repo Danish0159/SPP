ef50a17984c8880d9ae6ab95df00083fd5625629
Danish <danish01132@gmail.com>
2022-02-20 00:11:27 +0500

    Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import logo from "../images/logo2.png";
+import logo from "../images/logo3.png";
 
 const Navbar = ({ page }) => {
   if (page === "home") {
